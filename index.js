f948a78fb029504cd1bc43dcbdc922f953877a42
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-08-07 14:55:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = {
+    
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-unused-vars":"warn",
+        "object-curly-spacing": ["error", "always"]
+    }
+
+
+}
